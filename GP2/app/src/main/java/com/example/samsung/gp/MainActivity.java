e4ed3b5edb6992dd444596b0add70797d392aa29
MennaSala7 <menna.salah.moh@gmail.com>
2016-03-21 16:38:01 +0200

    Second version"signUpActivity"
    Menna

--- GP2/app/src/main/java/com/example/samsung/gp/MainActivity.java
+++ GP2/app/src/main/java/com/example/samsung/gp/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.samsung.gp;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import java.util.ArrayList;
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,45 +18,72 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+import com.example.samsung.gp.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner List;
+    private EditText FirstName;
+    private EditText LastName;
+    private EditText Email;
+    private EditText Password;
+    private EditText ConfirmPassword;
+    private Button StartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setTitle("Sign Up");
+
+        List = (Spinner) findViewById(R.id.spinner1);
+        FirstName=(EditText) findViewById(R.id.txtname);
+        LastName=(EditText) findViewById(R.id.txtlastname);
+        Email=(EditText) findViewById(R.id.txtemail);
+        Password=(EditText) findViewById(R.id.txtpassword);
+        ConfirmPassword=(EditText) findViewById(R.id.txtpassword2);
+        StartButton=(Button) findViewById(R.id.startbutton);
+
+        ArrayAdapter<String> adapter;
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Maadi");
+        list.add("Nasr City");
+        list.add("Shoubra");
+        list.add("Helwan");
+        list.add("Heliopolis");
+        adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,
+                R.id.txtSpinner_item, list);
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+        List.setAdapter(adapter);
+
+
+        StartButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
