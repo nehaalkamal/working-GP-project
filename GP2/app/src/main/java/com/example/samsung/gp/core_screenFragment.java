2aa097247a7ab24596251057724adc6966817727
nehaalkamal <nehaalkamal@gmail.com>
2016-04-27 23:37:21 +0200

    test

--- GP2/app/src/main/java/com/example/samsung/gp/core_screenFragment.java
+++ GP2/app/src/main/java/com/example/samsung/gp/core_screenFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -22,12 +23,12 @@ public class core_screenFragment extends Fragment {
 
     private Spinner AreaList;
     private EditText budget;
-    private ImageButton foodButton;
-    private ImageButton cafeButton;
-    private ImageButton cinemaButton;
-    private ImageButton entertainmentButton;
-    private ImageButton ahwaButton;
-    private ImageButton concertsButton;
+    private CheckBox foodButton;
+    private CheckBox cafeButton;
+    private CheckBox cinemaButton;
+    private CheckBox entertainmentButton;
+    private CheckBox ahwaButton;
+    private CheckBox dessertButton;
     private Button  yallaButton ;
     public core_screenFragment() {
     }
@@ -57,12 +58,12 @@ public class core_screenFragment extends Fragment {
         final ArrayList<String> choices = new ArrayList<String>();
 
         budget = (EditText) rootView.findViewById(R.id.budgetvalue);
-        foodButton=(ImageButton)rootView.findViewById(R.id.food);
-        cafeButton=(ImageButton)rootView.findViewById(R.id.cafe);
-        entertainmentButton=(ImageButton)rootView.findViewById(R.id.game);
-        cinemaButton=(ImageButton)rootView.findViewById(R.id.cinema);
-        ahwaButton=(ImageButton)rootView.findViewById(R.id.ahwa);
-        concertsButton=(ImageButton)rootView.findViewById(R.id.concert);
+        foodButton=(CheckBox)rootView.findViewById(R.id.food);
+        cafeButton=(CheckBox)rootView.findViewById(R.id.cafe);
+        entertainmentButton=(CheckBox)rootView.findViewById(R.id.game);
+        cinemaButton=(CheckBox)rootView.findViewById(R.id.cinema);
+        ahwaButton=(CheckBox)rootView.findViewById(R.id.ahwa);
+        dessertButton =(CheckBox)rootView.findViewById(R.id.dessert);
         yallaButton=(Button)rootView.findViewById(R.id.YallaButton);
 
 
@@ -145,14 +146,13 @@ public class core_screenFragment extends Fragment {
 
             }
         });
-        concertsButton.setOnClickListener(new View.OnClickListener() {
+        dessertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(choices.contains("others")){
+                if (choices.contains("others")) {
                     choices.remove("others");
                     v.setSelected(false);
-                }
-                else {
+                } else {
 
                     choices.add("others");
                     v.setSelected(true);
