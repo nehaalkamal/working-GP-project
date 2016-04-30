a650ac0b5243fc04af8acaa136da1c4513f8b0a6
MennaSala7 <menna.salah.moh@gmail.com>
2016-05-01 01:45:03 +0200

    Item Details "new"

--- GP2/app/src/main/java/com/example/samsung/gp/item_detailsFragment.java
+++ GP2/app/src/main/java/com/example/samsung/gp/item_detailsFragment.java
@@ -3,18 +3,34 @@ package com.example.samsung.gp;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
+
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class item_detailsFragment extends Fragment {
+
+
+    private TextView ItemName;
+    private TextView ItemPrice;
+    private TextView ItemAddress;
+    private TextView ItemRating;
+    private TextView OpenHours;
+    private TextView Cuisine;
+    private TextView Features;
+    private TextView Phone;
+    private ImageView ItemImage;
+
     String[] Items = {"boom boom boom", "boom boom boom", "boom boom boom"};
     String[] price = {"Menna Salah","Menna Salah","Menna Salah"};
     Integer[] image = {R.drawable.useravatar,R.drawable.useravatar,R.drawable.useravatar};
@@ -30,8 +46,30 @@ public class item_detailsFragment extends Fragment {
         ListView listItem = (ListView) rootView2.findViewById(R.id.commentslist);
         CustomArrayAdapter myAdapter = new CustomArrayAdapter(getActivity(), Items);
         listItem.setAdapter(myAdapter);
-        return  rootView2;
 
+
+        ItemName= (TextView)rootView2.findViewById(R.id.txtmainname);
+        ItemPrice=(TextView) rootView2.findViewById(R.id.txtpriceno);
+        ItemAddress=(TextView) rootView2.findViewById(R.id.txtLocation);
+        ItemRating=(TextView) rootView2.findViewById(R.id.txtrate);
+        OpenHours=(TextView) rootView2.findViewById(R.id.txtopenhoursin);
+        Cuisine=(TextView) rootView2.findViewById(R.id.txtcuisine);
+        Features=(TextView)rootView2.findViewById(R.id.txtfeaturesin);
+        Phone=(TextView)rootView2.findViewById(R.id.txtphoneno);
+        ItemImage=(ImageView)rootView2.findViewById(R.id.placelogo);
+
+
+
+        ItemName.setText("Frozzy Twist");
+        ItemPrice.setText("18");
+        ItemAddress.setText("15 Al-Bank St. (Inside Saudian Company Bldgs.)");
+        ItemRating.setText("8.6");
+        OpenHours.setText("1:00 pm to 1:00 am");
+        Cuisine.setText("Desserts");
+        Features.setText("WiFi, Outdoor");
+        Phone.setText("02 22876057");
+        ItemImage.setBackgroundResource(R.drawable.rsz_frozzy_twist);
+        return  rootView2;
     }
 
 
@@ -68,11 +106,11 @@ public class item_detailsFragment extends Fragment {
 
 
 
-             ImageView imageComment = (ImageView) rowView.findViewById(R.id.ImageComment); //imageComment Da feen fl xml?
-             TextView txtName = (TextView) rowView.findViewById(R.id.usercomment);
+            ImageView imageComment = (ImageView) rowView.findViewById(R.id.ImageComment); //imageComment Da feen fl xml?
+            TextView txtName = (TextView) rowView.findViewById(R.id.usercomment);
             //final TextView txtComment = (TextView) rowView.findViewById(R.id.txtcomment);
             TextView txtPrice = (TextView) rowView.findViewById(R.id.username);
-             ImageView imageView = (ImageView) rowView.findViewById(R.id.userphoto);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.userphoto);
             txtName.setText(values[position]);
             txtPrice.setText(price[position]);
             imageView.setImageResource(imageId[position]);
